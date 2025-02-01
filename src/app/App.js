cbd3c26f009e6b8a5975151be88ff60e00a39716
abdusalamovio <iskhakabdusalamov@mail.ru>
2025-02-02 00:34:26 +0500

    filter

--- src/app/App.js
+++ src/app/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
         { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 2 },
       ],
       term: "",
+      filter: "all",
     };
     this.maxId = 3;
   }
@@ -64,18 +65,33 @@ class App extends React.Component {
     this.setState({ term });
   };
 
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter((item) => item.rise);
+      case "moreThen1000":
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
   render() {
-    const { data, term } = this.state;
+    const { data, term, filter } = this.state;
     const employees = data.length;
     const increased = data.filter((item) => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
     return (
       <div className="app">
         <EmpInfo employees={employees} increased={increased} />
         <div className="empSearch">
           <EmpSearch onUpdateSearch={this.onUpdateSearch} />
-          <EmpFilter />
+          <EmpFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
         <EmpList
           data={visibleData}
--- src/components/empFilter/EmpFilter.js
+++ src/components/empFilter/EmpFilter.js
@@ -1,19 +1,28 @@
 import "./empFilter.css";
 
-const EmpFilter = () => {
-  return (
-    <div className="btn-group">
-      <button className="btn btn-light" type="button">
-        Все сотрудники
+const EmpFilter = (props) => {
+  const buttonsData = [
+    { name: "all", label: "Все сотрудники" },
+    { name: "rise", label: "На повышение" },
+    { name: "moreThen1000", label: "З/П больше 1000$" },
+  ];
+
+  const buttons = buttonsData.map(({ name, label }) => {
+    const active = props.filter === name;
+    const clazz = active ? "btn btn-light" : "btn btn-outline-light";
+    return (
+      <button
+        onClick={() => props.onFilterSelect(name)}
+        key={name}
+        className={clazz}
+        type="button"
+      >
+        {label}
       </button>
-      <button className="btn btn-outline-light" type="button">
-        На повышение
-      </button>
-      <button className="btn btn-outline-light" type="button">
-        З/П больше 1000$
-      </button>
-    </div>
-  );
+    );
+  });
+
+  return <div className="btn-group">{buttons}</div>;
 };
 
 export default EmpFilter;
