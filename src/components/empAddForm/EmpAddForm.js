8558f0a462f0b20cb8e5f32bfcf537dfafd4c284
abdusalamovio <iskhakabdusalamov@mail.ru>
2025-02-01 20:53:45 +0500

    controlled/uncontrolled components

--- src/components/empAddForm/EmpAddForm.js
+++ src/components/empAddForm/EmpAddForm.js
@@ -1,26 +1,50 @@
 import "./empAddForm.css";
+import React from "react";
 
-const EmpAddForm = () => {
-  return (
-    <div className="empAddForm">
-      <h3>Добавьте нового сотрудника</h3>
-      <form>
-        <input
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-          type="text"
-        />
-        <input
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-          type="number"
-        />
-        <button className="btn btn-outline-light" type="submit">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmpAddForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="empAddForm">
+        <h3>Добавьте нового сотрудника</h3>
+        <form>
+          <input
+            onChange={this.onValueChange}
+            value={name}
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            type="text"
+          />
+          <input
+            onChange={this.onValueChange}
+            value={salary}
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            type="number"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmpAddForm;
